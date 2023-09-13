73c2a7c2baefa90220032c7bc4e5abf5a391a9ef
AssanRabari <assandesai@gmail.com>
2023-09-13 23:19:16 +0530

    routes setup in all pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Style.scss";
+import HeroBanner from './heroBanner/HeroBanner';
 
-function home() {
+function Home() {
   return (
-    <div>home</div>
+    <div>
+      <HeroBanner />
+    </div>
   )
 }
 
-export default home
+export default Home
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Style.scss";
+
+function HeroBanner() {
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">Millions of movies,
+            movies, TV shows and people to discover.
+            Explore now</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default HeroBanner
